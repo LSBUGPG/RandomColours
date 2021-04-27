233307f3ab980a5d959ef6060fa60b2d7b01a807
Paul Sinnett <paul.sinnett@gmail.com>
2021-04-27 15:52:40 +0100

    Initial commit

---
+++ Assets/ColourRandomizer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourRandomizer : MonoBehaviour
+{
+    Material RandomFromList(List<Material> select)
+    {
+        int random = Random.Range(0, select.Count);
+        Material material = select[random];
+        select.RemoveAt(random);
+        return material;
+    }
+
+    public void Randomize(List<Material> colours, List<Material> faces)
+    {
+        List<Material> select = new List<Material>();
+        while (select.Count < faces.Count)
+        {
+            foreach (Material colour in colours)
+            {
+                select.Add(colour);
+                if (select.Count == faces.Count)
+                {
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < faces.Count; i++)
+        {
+            faces[i] = RandomFromList(select);
+        }
+    }
+}
---
+++ Assets/TestRandomizer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRandomizer : MonoBehaviour
+{
+    public List<Material> colours;
+    public List<Material> faces;
+    public ColourRandomizer randomizer;
+
+    void Start()
+    {
+        randomizer.Randomize(colours, faces);
+    }
+}
